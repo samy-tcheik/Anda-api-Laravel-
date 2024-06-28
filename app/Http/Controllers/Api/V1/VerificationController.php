4e89816a249b503c6dd900f61696daeb647d639c
samy tcheikman <samytcheikman@gmail.com>
2024-06-28 16:18:57 +0100

    Add Email verified messages

--- app/Http/Controllers/Api/V1/VerificationController.php
+++ app/Http/Controllers/Api/V1/VerificationController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Mail\VerficationMail;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 
@@ -18,7 +20,7 @@ class VerificationController extends Controller
         $user->verification_token = $verificationToken;
         $user->save();
 
-        $verificationLink = route("verify-email", ["token" => $verificationToken]);
+        $verificationLink = route("verify-email", ["token" => $verificationToken, "lang" => Lang::locale()]);
 
         // Send the verification link with the token via email
         Mail::to($user->email)->send(new VerficationMail($user, $verificationLink));
@@ -26,25 +28,23 @@ class VerificationController extends Controller
 
     public function verify(Request $request) {
         $token = $request->input('token');
+        $lang = $request->input('lang');
 
+        App::setLocale($lang);
         // Find the user with the verification token
         $user = User::where('verification_token', $token)->first();
 
-        if (!$user) {
-            response([
-                "message" => "user not found",
-                401
-            ]);
+        if (is_null($user) ) {
+            return view("auth.verification_token_expired");
         }
-
         if ($user->verification_token === $token) {
             $user->email_verified_at = now();
             $user->verification_token = null;
             $user->save();
 
-            return response()->json(['message' => 'Email verified successfully']);
+            return view("auth.verification_success");
         } else {
-            return response()->json(['message' => 'Invalid verification token'], 400);
+            return view("auth.verification_token_expired");
         }
     }
 }
